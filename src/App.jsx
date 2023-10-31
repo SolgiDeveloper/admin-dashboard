9a370f6e0c7b53c059d4dd2f73931270ac52ac08
solgi <mohammadsolgi96@gmail.com>
2023-10-31 12:44:39 +0330

    add login-register

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import Login from './features/identity/components/login'
+import Register from './features/identity/components/register'
 
 
 function App() {
 
   return (
-    <p>
-      platform 
-    </p>
+    <Register/>
   )
 }
 
