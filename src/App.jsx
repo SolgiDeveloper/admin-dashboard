f4e12c2371740f92fd90ce6f5380f013657dd046
solgi <mohammadsolgi96@gmail.com>
2023-11-19 17:34:52 +0330

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import './core/i18n';
-
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import "./core/i18n";
+import { useEffect } from "react";
+import {useAppContext} from "./contexts/app/app-context"
 function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  )
+  const { theme } = useAppContext();
+  useEffect(() => {
+    const head = document.head;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = `/css/${theme}.css`;
+    head.appendChild(link);
+    return () => {
+      setTimeout(()=>{
+        head.removeChild(link)
+      },100)
+      
+    }
+  }, [theme]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
